4f33588e06e56fddf95a914c0fe86c15027d0a23
Sean Okpala <seane8088@gmail.com>
2024-03-26 21:18:58 -0500

    add state to line 12 root index

--- store/Links.js
+++ store/Links.js
@@ -3,4 +3,7 @@ export default [
   { title: "Bio", text: "Bio" },
   { title: "Order", text: "Submit a Pizza Order" },
   { title: "Pizza", text: "See all the Pizzas" }
+  { title: "Pizza", text: "See all the Pizzas" }
+  { title: "Pizza", text: "See all the Pizzas" }
+  { title: "Pizza", text: "See all the Pizzas" }
 ];
