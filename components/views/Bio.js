2155603e7bb2b0effe8df26d933b4aa84f3db42d
Sean Okpala <seane8088@gmail.com>
2024-03-26 20:17:47 -0500

    installs axios and fixes syntax errors

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,18 +1,19 @@
 import html from "html-literal";
 
 export default () => html`
-export default () => html`
   <section id="bio">
-  <h1>About Me</h1>
-  <p>
-    From reading popular science magazines Im now learning to code
-  </p>
-</section>
+    <h1>About Me</h1>
+    <p>
+      From reading popular science magazines Im now learning to code
+    </p>
+  </section>
   ;
 
   <section id="bio">
-  <h1>About Me</h1>
-  <p>
-    From reading popular science magazines Im now learning to code
-  </p></section>
+    <h1>About Me</h1>
+    <p>
+      From reading popular science magazines Im now learning to code
+    </p>
+  </section>
   ;
+`;
